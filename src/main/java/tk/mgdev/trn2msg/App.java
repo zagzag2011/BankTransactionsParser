2c750d68e674aba871b7a3a92844a3f487c2707c
zagzag2011 <zagzag2011@gmail.com>
2022-09-08 19:13:57 +0300

    Init commit

---
+++ src/main/java/tk/mgdev/trn2msg/App.java
@@ -0,0 +1,13 @@
+package tk.mgdev.trn2msg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
